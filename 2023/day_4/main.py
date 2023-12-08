d3637862e7419a3bb8cb76ad0a2a90cb13804594
Leonard Lehner <leonard.lehner@hetzner.com>
2023-12-08 14:04:00 +0100

    game is game

--- 2023/day_4/main.py
+++ 2023/day_4/main.py
@@ -11,10 +11,11 @@ with open("data.txt") as data:
 pattern = re.compile(r'Card\s+(\d+):\s+((?:\d+\s+)+)\|\s+((?:\d+\s+)+\d+)')
 
 all_scores = []
+winning_cards = [1 for line in input_list]
 for i in range(len(input_list)):
     #print("###########")
     match = pattern.search(input_list[i])
-
+    counter = 0
     current_points = 0
     if match:
         card_number = match.group(1)
@@ -22,13 +23,19 @@ for i in range(len(input_list)):
         check_numbers = match.group(3).split()
         for num in winning_numbers:
             if num in check_numbers:
+                counter += 1
                 if current_points == 0:
                     current_points += 1
                 elif current_points > 0:
                     current_points *= 2
         #print(current_points)
         all_scores.append(current_points)
+        for x in range(0, counter):
+            if x <= len(input_list):
+                winning_cards[x + i + 1] += winning_cards[i]
+
 # Part 1
 print(sum(all_scores))
-
+# Part 2
+print(sum(winning_cards))
 
