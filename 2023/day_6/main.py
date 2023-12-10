3ca0acc5c9618c044656dd330d7683891117b061
30leo03 <leo.lehner03@gmail.com>
2023-12-10 12:54:35 +0100

    day 6 part 1

---
+++ 2023/day_6/main.py
@@ -0,0 +1,33 @@
+import re
+
+input_list = []
+with open("data.txt") as data:
+    for t in data:
+        line = t.strip()
+        input_list.append(line)
+
+pattern = re.compile(r':\s+((?:\d+\s+)+\d+)')
+total_time = [int(x) for x in pattern.search(input_list[0]).group(1).split()]
+distance_rec = [int(x) for x in pattern.search(input_list[1]).group(1).split()]
+
+record_beaters = []
+for t in range(len(total_time)):
+    possibilities = 0
+    for i in range(1, total_time[t]):
+        distance_travelled = (total_time[t] - i) * i
+        if distance_travelled > distance_rec[t]:
+            possibilities += 1
+    print(possibilities)
+    record_beaters.append(possibilities)
+result = 1
+for w in record_beaters:
+    result *= w
+
+# Part 1
+print(result)
+
+
+
+
+
+
